315501d0b2596e4ec4fa3d12e0b8477352479999
Lakshmi-Chakkungal-Balakrishnan <techrecruiter.lakshmi@gmail.com>
2024-11-30 16:41:25 -0800

    first commit

---
+++ Array/src/Array_pkg/Array.java
@@ -0,0 +1,19 @@
+package Array_pkg;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int a[]= {1,2,3,4,5,6,7,8,9}; 
+	/*	System.out.println(a[0]);
+	a[3]=55;
+	System.out.println(a[3]);*/ //arrays
+	
+	for(int i=0;i<a.length;i++)
+	{
+		System.out.println(a[i]);
+	}
+	}}
+
+
